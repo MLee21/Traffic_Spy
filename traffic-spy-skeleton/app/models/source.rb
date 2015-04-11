891d2c0a9f6f48e33e691e5a4e5724cbbec90547
MLee21 <minnie.lee89@gmail.com>
2015-04-10 22:16:06 -0600

    tested and wrote method for most requested urls to least requested urls.

--- traffic-spy-skeleton/app/models/source.rb
+++ traffic-spy-skeleton/app/models/source.rb
@@ -4,10 +4,8 @@ module TrafficSpy
     validates :identifier, uniqueness: :true, presence: true
     validates :root_url, presence: true
 
-    #based off the identifier, grab all payloads associated
-    # ** grab all data from each category **
-    #grab all the urls
-    #order by most to least
+    attr_reader :url_addresses
+   
     def self.url_index(id)
       related_payloads = Payload.where(source_id: id)
       ids = related_payloads.pluck(:url_id)
@@ -15,9 +13,32 @@ module TrafficSpy
         Url.where(id: id)
       end
       index = url_objects.map do |obj|
-        obj.pluck(:address).first
+        obj.pluck(:address)
       end
+      @url_addresses = index
     end
 
+    def self.most_requested_to_least_requested
+      remove_counts_from_address_array
+    end
+
+    private 
+
+    def self.group_by_frequency
+      @url_addresses.group_by{|x| x}
+    end
+
+    def self.count_frequency_of_each_address
+      group_by_frequency.map {|k,v| [k,v.size]}
+    end
+
+    def self.sort_addresses_from_most_to_least
+      count_frequency_of_each_address.sort_by(&:last).reverse
+    end
+
+    def self.remove_counts_from_address_array
+      r = sort_addresses_from_most_to_least.flatten
+      r.values_at(* r.each_index.select(&:even?))
+    end
   end
 end
--- traffic-spy-skeleton/test/models/source_test.rb
+++ traffic-spy-skeleton/test/models/source_test.rb
@@ -2,6 +2,13 @@ require './test/test_helper'
 
 class SourceTest < MiniTest::Test 
 
+  def create_payload(source, number, payload)
+    create = number.times do |x|
+      TrafficSpy::PayloadCreator.new(source, payload)
+    end
+    create.validate
+  end
+
   def test_source_has_urls
     raw_payload1 = ('{"url":"http://yolo.com/blog", "requestedAt":"2015-03-15 21:38:28 -0700", "respondedIn":32, "referredBy":"http://yourmom.com", "requestType":"GET", "parameters":[], "eventName": "YOLO", "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17", "resolutionWidth":"1920", "resolutionHeight":"1280", "ip":"62.23.37.212"}')
     raw_payload2 = ('{"url":"http://yolo.com/blog", "requestedAt":"2018-03-25 21:38:28 -0700", "respondedIn":20, "referredBy":"http://yourmom.com", "requestType":"GET", "parameters":[], "eventName": "YOLO", "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17", "resolutionWidth":"1920", "resolutionHeight":"1280", "ip":"62.23.37.212"}')
@@ -19,7 +26,37 @@ class SourceTest < MiniTest::Test
     source2 = TrafficSpy::Source.where(identifier: 'yolo')
     assert_equal Array, TrafficSpy::Source.url_index(source1[:id]).class
     assert_equal 3, TrafficSpy::Source.url_index(source1[:id]).count
-    assert_equal true, TrafficSpy::Source.url_index(source1[:id]).any? {|x| x == "http://yolo.com/blog"}
-    assert_equal true, TrafficSpy::Source.url_index(source1[:id]).any? {|x| x == "http://yolo.com/rolo"}
+  end
+
+  def test_urls_are_ordered_from_most_to_least_requested
+    raw_payload1 = ('{"url":"http://yolo.com/blog", "requestedAt":"2011-03-15 21:38:28 -0700", "respondedIn":32, "referredBy":"http://yourmom.com", "requestType":"GET", "parameters":[], "eventName": "YOLO", "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17", "resolutionWidth":"1920", "resolutionHeight":"1280", "ip":"62.23.37.212"}')
+    raw_payload2 = ('{"url":"http://yolo.com/blog", "requestedAt":"2014-02-25 21:38:28 -0700", "respondedIn":20, "referredBy":"http://yourmom.com", "requestType":"GET", "parameters":[], "eventName": "YOLO", "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17", "resolutionWidth":"1920", "resolutionHeight":"1280", "ip":"62.23.37.212"}')
+    raw_payload3 = ('{"url":"http://yolo.com/rolo", "requestedAt":"2013-03-15 21:38:28 -0700", "respondedIn":32, "referredBy":"http://yourmom.com", "requestType":"GET", "parameters":[], "eventName": "YOLO", "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17", "resolutionWidth":"1920", "resolutionHeight":"1280", "ip":"62.23.37.212"}')
+    raw_payload4 = ('{"url":"http://yolo.com/rolo", "requestedAt":"2012-04-15 21:38:28 -0700", "respondedIn":29, "referredBy":"http://yourmom.com", "requestType":"GET", "parameters":[], "eventName": "YOLO", "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17", "resolutionWidth":"1920", "resolutionHeight":"1280", "ip":"62.23.37.212"}')
+    raw_payload5 = ('{"url":"http://yolo.com/rolo", "requestedAt":"2013-07-15 21:38:28 -0700", "respondedIn":31, "referredBy":"http://yourmom.com", "requestType":"GET", "parameters":[], "eventName": "YOLO", "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17", "resolutionWidth":"1920", "resolutionHeight":"1280", "ip":"62.23.37.212"}')
+    raw_payload6 = ('{"url":"http://yolo.com/solo", "requestedAt":"2013-09-15 21:38:28 -0700", "respondedIn":65, "referredBy":"http://yourmom.com", "requestType":"GET", "parameters":[], "eventName": "YOLO", "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17", "resolutionWidth":"1920", "resolutionHeight":"1280", "ip":"62.23.37.212"}')
+    
+    source1 = TrafficSpy::Source.create({identifier: "yolo", root_url: "http://yolo.com"})
+    payload_creator = TrafficSpy::PayloadCreator.new(source1, raw_payload1)
+    payload_creator.validate
+
+    payload_creator = TrafficSpy::PayloadCreator.new(source1, raw_payload2)
+    payload_creator.validate
+
+    payload_creator = TrafficSpy::PayloadCreator.new(source1, raw_payload3)
+    payload_creator.validate
+
+    payload_creator = TrafficSpy::PayloadCreator.new(source1, raw_payload4)
+    payload_creator.validate
+
+    payload_creator = TrafficSpy::PayloadCreator.new(source1, raw_payload5)
+    payload_creator.validate
+
+    payload_creator = TrafficSpy::PayloadCreator.new(source1, raw_payload6)
+    payload_creator.validate
+
+    assert_equal 6, TrafficSpy::Source.url_index(source1[:id]).count
+    assert_equal "http://yolo.com/rolo", TrafficSpy::Source.most_requested_to_least_requested.first
+    assert_equal "http://yolo.com/solo", TrafficSpy::Source.most_requested_to_least_requested.last
   end
 end
